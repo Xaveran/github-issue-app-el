7782d9a07d3536abc1e363b5e5d32c3fba55c58c
Przemek Bełczyk <pbl@touk.pl>
2019-12-29 18:52:11 +0100

    Layout center by margin

--- src/containers/layout/Layout.styled.js
+++ src/containers/layout/Layout.styled.js
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 
+const widthInEm = 40;
+const heightInEm = 40;
+
 export const Layout = styled.div`
   position: absolute;
-  top: 20%;
-  left: 35%;
-  width: 500px;
-  height: 500px;
+  top: 50%;
+  left: 50%;
+  width: ${widthInEm}em;
+  height: ${heightInEm}em;
   background-color: white;
-  margin: auto;
+  margin-top: -${heightInEm / 2}em;
+  margin-left: -${widthInEm / 2}em;
 `;
